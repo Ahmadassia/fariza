22d27b091daa432009ff87b1e0b7c45e79fb13d8
bilal <bilal.m.saed@gmail.com>
2021-03-01 22:12:16 +0300

    crate crud author article

--- application/controllers/Author.php
+++ application/controllers/Author.php
@@ -7,7 +7,118 @@ class Author extends MY_Controller {
 		parent::__construct();
 		$this->load->model('getdb');
 	}
+	public function form(){
+		$id = $this->input->post('id');
+		if($id != '' and $id > 0){
+
+			$dbs['from'] = 'article';
+			$dbs['where']['id_article'] = $id;
+			$dbs['where']['id_author_fk'] = $this->session->userdata('authorstate');
+			$data['data'] = $this->getdb->getall($dbs);
+			if(count($data['data']) === 1){
+				$data['data'] = $data['data'][0];
+			}
+			$this->load->view('author/form',$data);
+		} else {
+			$this->load->view('author/form');
+		}
+	}
 	
+	public function getdata($id = ''){
+		$dbs['select'] = "*,GROUP_CONCAT(category_name) as cates";
+		$dbs['from'] = 'article ar';
+		$dbs['join'] = [
+			'article_cate ac' => 'ac.id_article_fk = ar.id_article',
+			'category ca' => 'ca.idcate = ac.idcate_fk'
+		];
+		$dbs['group'] = ['ar.id_article'];
+		$dbs['sort'] = ['ar.id_article' => 'desc'];
+		if($id != '' and $id > 0){
+			$dbs['where']['id_article'] = $id;
+		}
+		$dbs['where']['id_author_fk'] = $this->session->userdata('authorstate');
+		return $this->getdb->getall($dbs);
+	}
+	public function article(){
+		$get = $this->input->post('get');
+		if($get === "1"){
+			$data['list'] = $this->getdata();
+			$this->load->view('author/list_tbl', $data);
+		} else {
+
+		$id = $this->input->post('id');
+		$data = $this->input->post('data');
+		$category = $this->input->post('category');
+		$this->load->library('form_validation');
+
+		$this->form_validation->set_rules('data[title]', 'ناونیشان', 'trim|required', [
+			'required'=> 'تكایه‌ %s پڕبكه‌ره‌وه‌ .']
+		);
+		$this->form_validation->set_rules('data[content]', 'بابەت', 'trim|required', [
+			'required'=> 'تكایه‌ %s پڕبكه‌ره‌وه‌ .']
+		);
+		
+		$src = './assets/article/';
+		if ($this->form_validation->run() == False) {
+			$json['msg'] = validation_errors();
+			$json['class'] = 'warning';
+			$json['states'] = 'failed';
+		} else {
+			$data = html_escape($data);
+			
+			$config['upload_path']          = $src;
+	        $config['allowed_types']        = 'gif|jpg|png|jpeg';
+			$config['file_name'] = date('Y-m-d-Hms');
+			$this->load->library('upload', $config);
+			
+
+			if($id != '' and $id > 0){ // update
+				if ($this->upload->do_upload('image')) {
+
+	            	$oldimg = $this->input->post('old_image');
+	            	$oldimg = $oldimg ?? "aa.jpg";
+
+	            	if (file_exists($src.$oldimg)) {
+	            		unlink($src.$oldimg);
+	            	}
+	                $dataimg = $this->upload->data();
+	                $data['article_image'] = $dataimg['file_name'];
+	            }
+				$res = $this->getdb->updatedb('article',$data,$id,'id_article');
+				$res = $this->getdb->deletedb($id,'article_cate','id_article_fk');
+				$cates = [];
+				foreach ($category as $key => $value) {
+					$cates[] = ['id_article_fk' => $id,'idcate_fk' => $value];
+				}
+				$res = $this->getdb->insertdbba('article_cate',$cates);
+			} else { // insert
+				if ( ! $this->upload->do_upload('image')){
+					$json['msg'] = $this->upload->display_errors();
+				} else {
+					$dataimg = $this->upload->data();
+					$data['article_image'] = $dataimg['file_name'];
+				}
+				$data['id_author_fk'] = $this->session->userdata('authorstate');
+				$res = $this->getdb->insertdb('article',$data);
+				$cates = [];
+				foreach ($category as $key => $value) {
+					$cates[] = ['id_article_fk' => $res,'idcate_fk' => $value];
+				}
+				$res = $this->getdb->insertdbba('article_cate',$cates);
+			}
+			if($res > 0){
+				$json['msg'] = 'بەسەرکەوتویی جێبەجێ کرا';
+				$json['class'] = 'success';
+				$json['states'] = 'ok';
+			} else {
+				$json['msg'] = 'دوبارە هەوڵبدەرەوە!';
+				$json['class'] = 'warning';
+				$json['states'] = 'failed';
+			}
+		}
+		echo json_encode($json);
+	}
+	}
 	public function index(){
 		if($this->session->userdata('authorstate') !== null){
 
@@ -84,6 +195,23 @@ class Author extends MY_Controller {
 		echo json_encode($json);
 
 	}
+	public function removerec(){
+		$id = $this->input->post('id');
+		$json['msg'] = 'دوبارە هەوڵبدەرەوە!';
+		$json['class'] = 'warning';
+		$json['states'] = 'failed';
+		if($id != '' and $id > 0){
+			$res = $this->getdb->deletedb($id,'article','id_article');
+			if($res > 0){
+				$json['msg'] = 'بەسەرکەوتویی جێبەجێ کرا';
+				$json['class'] = 'success';
+				$json['states'] = 'ok';
+			}
+		}
+		echo json_encode($json);
+	}
+
+	
 }
 
 /* End of file author.php */
---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+	$('body').on('click','.removeRec', function () {
+		if(confirm('دڵنیای له‌سڕینه‌وه‌ی؟')){
+			var id = $(this).data('id');
+			var page = $(this).data('page');
+			var th = $(this).parent().parent();
+			$.post(mainUrl+page, {id: id}, function(data) {
+				data = JSON.parse(data);
+				if(data.states == "ok"){
+					$(th).hide('slow');
+				}
+				$('body').toast({
+					class: data.class,
+					message: data.msg,
+					displayTime: 6000
+				},'json');
+			});
+		}
+	});
+    $('body').on('submit','#modalformall',function(e){
+		e.preventDefault();
+		$('#subbtn').attr('disabled', 'disabled');
+		var form = $('#modalformall').get(0); 
+		
+		var formSendda = $(form).data('sent');
+		var formdata = new FormData(form);
+		
+		var req = new XMLHttpRequest();
+		
+		req.addEventListener('load',function(e){
+			var data = {};
+			try {
+				data = JSON.parse(e.currentTarget.response);
+
+				if(data.states == "ok"){
+					if(typeof data.redirect != "undefined"){
+						location.href = mainUrl+data.redirect;	
+					} else {
+						$.post(mainUrl+formSendda+"/getdata", {get: 1}, function(getdata) {
+							$('#showdata').html(getdata)
+							$('.ui.modal').modal('hide');
+						});
+					}
+				}
+				$('body').toast({
+					class: data.class,
+					message: data.msg,
+					displayTime: 6000
+				});
+			} catch (error) {
+
+			}
+			
+			$('#subbtn').removeAttr('disabled');
+
+		});
+		req.open('post',mainUrl+formSendda+"/action",'true');
+		req.send(formdata);
+		return false;
+	});
+});
