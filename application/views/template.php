6cb4a064127f81b66bb12f9211872817728bfb05
bilal <bilal.m.saed@gmail.com>
2021-03-01 20:35:41 +0300

    setup semantic ui and jquery library

---
+++ application/views/template.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ku" dir="rtl">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link rel="icon" href="<?=base_url('assets/img/'.$this->webop['logo'])?>" type="image/jpeg" sizes="16x16">
+    <title><?=$this->webop['web_title']?></title>
+    <link href="https://fonts.googleapis.com/earlyaccess/notonaskharabic.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href=<?=base_url("assets/semantic.min.css");?>>
+    <link rel="stylesheet" type="text/css" href=<?=base_url("assets/css/stylead.css");?>>
+    <link rel="stylesheet" type="text/css" href=<?=base_url("assets/css/suic.css");?>>
+    
+    <script>var mainUrl = '<?=base_url();?>'</script>
+    
+</head>
+<body>
+    
+    
+    <script src="<?=base_url('assets/js/jquery.min.js')?>"></script>
+    <script src="<?=base_url('assets/semantic.min.js')?>"></script>
+    <script src="<?=base_url('assets/js/main.js')?>"></script>
+      
+</body>
+</html>
